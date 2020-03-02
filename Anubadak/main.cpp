7f2765e1b6d703ee07813d1710b22670ff67f01e
Aaditya Subedi <aaditya@email.com>
2020-03-02 20:27:07 +0545

    repl

--- Anubadak/main.cpp
+++ Anubadak/main.cpp
@@ -9,8 +9,11 @@
 #include "visitor/interpreter.h"
 int main()
 {
+
+	
 	/*ifstream& file;
 	file.open("file.txt");*/
+	/*
 	SemanticScope semanticGlobalScope;
 	InterpreterScope interpreterGlobalScope;
 	std::cout << "Anubadak and Interpreter";
@@ -20,7 +23,6 @@ int main()
 	{
 		std::string input_line;
 		std::string program;
-		bool expr = false;
 		std::cout << "\n>>>  ";
 		std::getline(std::cin, input_line);
 		// removes the leading and trailing spaces, regex is used .
@@ -71,7 +73,7 @@ int main()
 		parser::Parser parser(&lexer);
 		try {
 
-			// Tokenise and initialise parser
+		
 			lexer::Lexer lexer(program);
 			//---------------A
 			std::cout << "lexing task completed \n";
@@ -81,63 +83,110 @@ int main()
 
 			parser::ASTProgramNode* prog;
 
-			// Try to parse as program
+	
 			try {
 				prog = parser.parse_program();
 			}
 
-			// Catch by trying to parse as expression
 			catch (const std::exception & e) {
 
 				try {
-					// If expression ends with ';', get rid of it
+					
 					if (program.back() == ';')
 						program.pop_back();
 
-					// Parse again, create program node manually
+				
 					lexer::Lexer expr_lexer(program);
-					parser = parser::Parser(&expr_lexer, 0);  // do not consume first token
+					parser = parser::Parser(&expr_lexer, 0);  
 					prog = new parser::ASTProgramNode(
 						std::vector<parser::ASTNode*>({ parser.parseExpression() }));
-
-					expr = true;
 				}
 				catch (const std::exception & expr_e) {
 
-					// Throw original error
+	
 					throw std::runtime_error(e.what());
 				}
 			}
-			/*
+		
 			// Generate XML
-			visitor::XMLVisitor xml_generator;
-			xml_generator.visit(prog);
-			*/
+			//visitor::XMLVisitor xml_generator;
+			//xml_generator.visit(prog);
+			
 
 		
 			SemanticScope temp = semanticGlobalScope;
 			SemanticAnalyser tempSemanticAnalyser(&temp);
 			tempSemanticAnalyser.visit(prog);
 
-			// If this succeeds, perform semantic analysis modifying global scope
-			SemanticAnalyser semanticAnalyser(&semanticGlobalScope);
+	
+			SemanticAnalyser semanticAnalyser(&semanticGlobalScope);  
 			semanticAnalyser.visit(prog);
 
-			// Interpreter
 			Interpreter interpreter(&interpreterGlobalScope);
 			interpreter.visit(prog);
 
 
 		}
 
-		
 
-		// Catch exception and print error
 		catch (const std::exception & e) {
 			std::cerr << e.what() << std::endl;
 		}
 
 	}
 	return 0;
+}
+
+
+*/
+
+std::ifstream inputFile;
+inputFile.open("myprogram.txt");
+std::string program;
+std::string tempString;
+while (std::getline(inputFile, tempString)) {
+	program.append(tempString);
+	program.append("\n");
+}
+if (DEBUG == true) {
+	std::cout << "the program is  shown below ......" << std::endl;
+	std::cout << program << std::endl;
+}
+//std::string program=
+//	" //this is comment \n def func(x: int):int \n  {print(\" i m the function \");\nreturn 2*x;} \n var y: int=func(9); print(\"hello there \\n\");\n print(y);";
+lexer::Lexer lexer(program);
+if (DEBUG) {
+	std::cout << "LENGTH OF TOKENS=" << lexer.tokens.size() << std::endl;
+	for (size_t i = 0; i < lexer.tokens.size(); i++)
+	{
+		std::cout << "token = " << lexer.tokens[i].getVal() << " \t\t\t\t and   type =" << lexer.tokens[i].getType() << std::endl;
+	}
+}
+parser::ASTProgramNode* prog = nullptr;
+parser::Parser parser(&lexer);
+try {
+
+	prog = parser.parse_program();
+}
+catch (std::exception & exception)
+{
+	std::cerr << "error :" << exception.what() << std::endl;
+}
+try {
+	SemanticAnalyser semantic;
+	semantic.visit(prog);
+}
+catch (std::exception & e) {
+	std::cerr << "ERROR:: semantic analysisis error occured  description:: " << e.what() << std::endl;
+}
+
+try {
+	Interpreter interpreter;
+	interpreter.visit(prog);
+}
+catch (std::exception & e) {
+	std::cerr << "interprete ERROR:" << e.what() << std::endl;
+}
+return 0;
 }
 
